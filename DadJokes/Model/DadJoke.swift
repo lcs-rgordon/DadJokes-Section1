2315351a172f9ce1772876623aea4474c73c74e8
Russell Gordon <github@russellgordon.ca>
2022-02-22 09:44:08 -0500

    We can load a joke from the remote endpoint when the app opens.

---
+++ DadJokes/Model/DadJoke.swift
@@ -0,0 +1,15 @@
+//
+//  DadJoke.swift
+//  DadJokes
+//
+//  Created by Russell Gordon on 2022-02-22.
+//
+
+import Foundation
+
+struct DadJoke: Decodable {
+    let id: String
+    let joke: String
+    let status: Int
+}
+
